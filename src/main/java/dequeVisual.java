ab1b9856c9de5f29ad25ac0a750f773a542df1ad
huskydj1 <42313131+huskydj1@users.noreply.github.com>
2020-10-23 21:32:02 -0700

    Create classes for new data structures and mark future plans with a TODO

---
+++ src/main/java/dequeVisual.java
@@ -0,0 +1,6 @@
+public class dequeVisual {
+    public static void main(String[] args) {
+        //TODO: Implement a deque visualizer. Parameterize the appearance of both ends in order to make stack/queue visual easier
+        //Particularly for deque operations, consider implementing a "scoot" animation
+    }
+}
